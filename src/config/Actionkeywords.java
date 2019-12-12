699092c668c1993d9f9c68d12939e916eef5c6e3
ammanrr <ammanrr@HYDVMIOAMMANRR1.corp.amvescap.net>
2019-12-12 13:51:59 +0530

    This is My first Framework

---
+++ src/config/Actionkeywords.java
@@ -0,0 +1,103 @@
+package config;
+
+import java.util.concurrent.TimeUnit;
+import static executionEngine.DriverScript.OR;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
+
+import executionEngine.DriverScript;
+//import org.openqa.selenium.firefox.FirefoxDriver;
+import utility.Log;
+
+public class Actionkeywords {
+
+		public static WebDriver driver;
+	
+	//All the methods in this class now accept 'Object' name as an argument
+	public static void openBrowser(String object,String data){	
+		try{
+			if(data.equals("Mozilla")) {
+				driver=new FirefoxDriver();
+				Log.info("Moxilla browser opened");
+			}
+			else if(data.equals("IE")) {
+				driver=new InternetExplorerDriver();
+				Log.info("Moxilla browser opened");
+			}
+			else if(data.equals("chrome"))
+			{
+				
+		System.setProperty("webdriver.chrome.driver","C://Users//ammanrr.CORP//Downloads//chromedriver.exe");
+   	    driver = new ChromeDriver(); 
+   	    driver.manage().window().maximize();
+   	    driver.manage().deleteAllCookies();
+   	    driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+   	    Log.info("chrome browser opened");
+		}}catch(Exception e){
+			Log.info("Not able to open Browser --- " + e.getMessage());
+			DriverScript.bResult = false;
+			}
+		}
+
+	public static void Navigate(String object,String data)
+	{	try {
+		Log.info("Navigate to URL");
+		
+		driver.get(Constants.URL);}
+		catch(Exception e) {
+			Log.info("Not able to navigate --- " + e.getMessage());
+			DriverScript.bResult = false;
+		}
+		}
+
+	
+	
+	public static void input_UserName(String object,String data){try {
+		Log.info("Enter userID");
+		driver.findElement(By.xpath(OR.getProperty(object))).sendKeys(data); 
+		}catch(Exception e){
+			 Log.error("Not able to Enter UserName --- " + e.getMessage());
+			 DriverScript.bResult = false;
+		 	}}
+
+	public static void input_Password(String object,String data){
+		try {
+		Log.info("Enter password");
+		driver.findElement(By.xpath(OR.getProperty(object))).sendKeys(data);
+		}catch(Exception e){
+			 Log.error("Not able to Enter Password --- " + e.getMessage());
+			 DriverScript.bResult = false;
+		 	}}
+
+	public static void waitFor(String object) throws Exception{
+		try {
+		Log.info("waiting");
+		Thread.sleep(5000);
+		}catch(Exception e){
+			 Log.error("Not able to Wait --- " + e.getMessage());
+			 DriverScript.bResult = false;
+        	}}
+
+	public static void click_Login(String object,String Data)
+	{
+		try {
+		Log.info("Click on log button");
+		driver.findElement(By.xpath(OR.getProperty(object))).click();
+	}catch(Exception e){
+		 Log.error("Not able to login --- " + e.getMessage());
+		 DriverScript.bResult = false;
+   	}}
+	public static void closeBrowser(String object,String Data){
+		try {
+		Log.info("Closing the browser");
+		driver.quit();
+		}catch(Exception e){
+			 Log.error("Not able to Close the Browser --- " + e.getMessage());
+			 DriverScript.bResult = false;
+        	}}
+
+}
---
+++ src/config/Constants.java
@@ -0,0 +1,35 @@
+package config;
+
+
+
+public class Constants {
+
+	//This is the list of System Variables
+    //Declared as 'public', so that it can be used in other classes of this project
+    //Declared as 'static', so that we do not need to instantiate a class object
+    //Declared as 'final', so that the value of this variable can be changed
+    // 'String' & 'int' are the data type for storing a type of value	
+	public static final String URL = "http://ushoucwsys01vt/logon";
+	public static final String Path_TestData = "C://Users//ammanrr.CORP//Test2.xlsx";
+	public static final String Path_OR="C://Users//ammanrr.CORP//eclipse-workspace//Framework//src//config//OR.txt";
+	public static final String File_TestData = "Test2.xlsx";
+	public static final String KEYWORD_FAIL = "FAIL";
+	public static final String KEYWORD_PASS = "PASS";
+
+	//List of Data Sheet Column Numbers
+	public static final int Col_TestCaseID = 0;	
+	public static final int Col_TestScenarioID =1 ;
+	public static final int Col_PageObject =4 ;
+	public static final int Col_ActionKeyword =5 ;
+	public static final int Col_Results=3;
+	public static final int Col_DataSet =6 ;
+	public static final int Col_TestStepResult =7 ;
+	public static final int Col_Runmode=2;
+	
+	//List of Data Engine Excel sheets
+	public static final String Sheet_TestSteps = "Test Steps";
+	public static final String Sheet_TestCases="Test Cases";
+	public static final String Sheet_TestData="Test Data";
+	// List of Test Data
+	public static final String UserName = "ammanrr";
+	public static final String Password = "ammanrr";}
